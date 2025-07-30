7c89b403f124eec1c777ccce4b423470e4bdb5e0
Hadi Winoto <mulyo.hadi@gunanusa.co.id>
2025-07-30 11:05:43 +0700

    update

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -36,7 +36,7 @@ const LoginForm = () => {
             padding: 20,
             boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
             borderRadius: 1,
-            border: '1px solid #ddd',
+            // border: '1px solid #ddd',
             backgroundColor: 'white',
           },
         }}
